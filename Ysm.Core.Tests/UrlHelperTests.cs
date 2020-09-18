14be3a61b3f05d5e0f3f7fb4c94fd1e3c5f25414
Kadosa Peter <kadosa.peter@outlook.com>
2020-09-18 07:00:16 +0200

    Write a few demo tests.

---
+++ Ysm.Core.Tests/UrlHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace Ysm.Core.Tests
+{
+    public class UrlHelperTests
+    {
+        [Fact]
+        public void GetFeedUrl_Should_Return_Valid_Result()
+        {
+            // Arrange
+            string expected = "https://www.youtube.com/feeds/videos.xml?channel_id=UCNd0qqcBpuXCWPM76lDUxqg";
+
+            string channelId = "UCNd0qqcBpuXCWPM76lDUxqg";
+
+            // Act
+            string result = UrlHelper.GetFeedUrl(channelId);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GetFeedUrl_Should_Return_Null_For_Empty_String()
+        {
+            // Arrange
+            string channelId = "";
+
+            // Act
+            string result = UrlHelper.GetFeedUrl(channelId);
+
+            // Assert
+            Assert.Null(result);
+        }
+    }
+}
